ec33a2dbc619e4134e89b88fecff4d16da1e758a
The Raider <53000629+raiderss@users.noreply.github.com>
2024-05-06 03:07:44 +0300

    Update shared.lua
    rep ty ESCLOSER

--- main/shared.lua
+++ main/shared.lua
@@ -18,17 +18,26 @@ Config = {
     }
 }
 
-function GetFramework()
+function GetFramework() -- eyw knk cözdüm
+    local Get = nil
     if Config.Framework == "ESX" then
-        return exports['esx']:getSharedObject()
-    elseif Config.Framework == "NewESX" then
-        return exports['es_extended']:getSharedObject()
-    elseif Config.Framework == "QBCore" then
-        return exports["qb-core"]:GetCoreObject()
-    elseif Config.Framework == "OldQBCore" then
-        return exports["old-qb-core"]:GetCoreObject()
+        while Get == nil do
+            TriggerEvent('esx:getSharedObject', function(Set) Get = Set end)
+            Citizen.Wait(0)
+        end
     end
+    if Config.Framework == "NewESX" then
+        Get = exports['es_extended']:getSharedObject()
+    end
+    if Config.Framework == "QBCore" then
+        Get = exports["qb-core"]:GetCoreObject()
+    end
+    if Config.Framework == "OLDQBCore" then
+        while Get == nil do
+            TriggerEvent('QBCore:GetObject', function(Set) Get = Set end)
+            Citizen.Wait(200)
+        end
+    end
+    return Get
 end
-
-
 
